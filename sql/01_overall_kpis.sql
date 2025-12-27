e885c61243934a2d288a02e06626ef9dd874d8ff
haleh-bozorg <haleh.bozorg@gmail.com>
2025-12-26 18:20:55 -0800

    Update 01_overall_kpis.sql

--- sql/01_overall_kpis.sql
+++ sql/01_overall_kpis.sql
@@ -1,1 +1,12 @@
+-- =====================================================
+-- KPI 1: Overall Sales Performance
+-- Description:
+--   Calculates total revenue, total invoices,
+--   and number of unique customers.
+-- =====================================================
 
+SELECT
+  ROUND(SUM(total), 2)        AS total_revenue,
+  COUNT(invoice_id)           AS total_invoices,
+  COUNT(DISTINCT customer_id) AS unique_customers
+FROM invoice;
